a59b1516ec34af7756508a4fd8b07cd51bafa5e0
Joel <7258285+ramosjoel@users.noreply.github.com>
2024-03-29 16:41:35 -0700

    Adds kubectl setup

--- setup.sh
+++ setup.sh
@@ -156,7 +156,13 @@ sudo hdiutil detach /Volumes/Docker
 ## Open iTerm and go to Profiles > Colors, and import the Color Presets... dropdown to import the downloaded file.
 curl https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/GitHub%20Dark.itermcolors -o GitHub-Dark.itermcolors
 
-# Kubernetes (k8s)
+## Kubernetes (k8s)
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl"
+# Verify checksum (should print "kubectl: OK")
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | shasum -a 256 --check
+# Move kubectl to /usr/local/bin/
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo chown root: /usr/local/bin/kubectl
+
